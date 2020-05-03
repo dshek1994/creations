594de457e5c9b56b9f29a81d2355755dec2c29a5
dshek1994 <danielshekari94@gmail.com>
2020-05-03 01:17:55 -0500

    Created fieldsets

--- websiteproject1/main/admin.py
+++ websiteproject1/main/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Tutorial
 
 class TutorialAdmin(admin.ModelAdmin):
-    fields = ["tutorial_title",
-    "tutorial_published", "tutorial_content"]
+    fieldsets = [
+        ("Title/date", {'fields': ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
 
 admin.site.register(Tutorial,TutorialAdmin)
