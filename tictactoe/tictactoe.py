41c8da8b3c0f4a6b313a60066e990d4a6f0928b1
dshek1994 <danielshekari94@gmail.com>
2020-06-12 21:03:35 -0500

    Created more functions for win conditions

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -12,9 +12,32 @@ def play_game():
     # Display initial board
     display_board()
 
-    handle_turn()
+    while game_still_going:
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
 
 def handle_turn():
     position = input("Choose a position from 1-9: ")
+    postion = int(position) - 1
+
+    board[postion] = "X"
+    display_board()
+
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+def check_if_win():
+    # check rows
+    # check columns
+    #check diagonals
+    return
+
+def check_if_tie():
+    return
+
 
 play_game()
