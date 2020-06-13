d711d9d7c1c2e08f108a1ea5c9748106dc83827d
dshek1994 <danielshekari94@gmail.com>
2020-06-12 21:26:18 -0500

    Created more comments to the code

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -19,16 +19,21 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
+# Play a game of tic tac toe
 def play_game():
 
     # Display initial board
     display_board()
 
     while game_still_going:
+
+        # handle a single turn of an arbitrary player
         handle_turn(current_player)
 
+        # check if the game has ended
         check_if_game_over()
 
+        # Flip to the other player
         flip_player()
     
     # The game has ended
@@ -37,6 +42,7 @@ def play_game():
     elif winner == None:
         print("Tie.")
 
+# Handle a single turn of an arbirtrary player
 def handle_turn(player):
     position = input("Choose a position from 1-9: ")
     postion = int(position) - 1
